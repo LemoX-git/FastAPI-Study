012414bc915505ae2ff51663de8caabe0156f35a
Berezhnoy Vladimir <vaberezhnoy@mail.ru>
2026-05-21 16:30:39 +0300

    fix: Исправлены импорты в моделях.

--- lesson 6/fastapi_ecommerce/app/models/categories.py
+++ lesson 6/fastapi_ecommerce/app/models/categories.py
@@ -2,6 +2,7 @@ from sqlalchemy import Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
+from app.models.products import Product
 
 
 class Category(Base):
--- lesson 6/fastapi_ecommerce/app/models/products.py
+++ lesson 6/fastapi_ecommerce/app/models/products.py
@@ -4,6 +4,7 @@ from sqlalchemy import String, Boolean, Integer, Numeric, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
+from app.models.categories import Category
 
 
 class Product(Base):
